86fd9bfaaff066b10c8dbc5fa8002a378551fcc1
Munir Hussain <munirkamboh005@gmail.com>
2026-08-23 06:35:00 +0500

    virsion 1.0.1

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const navLinks = document.querySelectorAll("nav ul li a");
 const togglerBtn = document.querySelector(".toggler");
 const linkedSection = document.querySelectorAll(".linked-section");
 const BackToTopBtn = document.querySelector(".back-to-top");
+const skillsCard = document.querySelectorAll(".skill-card");
+const form = document.querySelector("form");
+const fullNameInput = document.querySelector("#fullName");
+const mailInput = document.querySelector("#mail");
+const messageTextarea = document.querySelector("#message");
+const formBtn = document.querySelector(".form-btn");
+const nameError = document.querySelector(".name-error");
+const mailError = document.querySelector(".mail-error");
+const messageError = document.querySelector(".message-error");
+const submittedMessage = document.querySelector(".submit-message");
 
 //   ------     Move the dot with cursor
 const mouseDot = document.querySelector(".mouse-dot");
@@ -32,7 +42,9 @@ window.addEventListener("scroll", () => {
 
   //   show the back to top button on scroll
 
-  window.outerHeight < window.scrollY ? BackToTopBtn.classList.add("show") : BackToTopBtn.classList.remove("show");
+  window.outerHeight < window.scrollY
+    ? BackToTopBtn.classList.add("show")
+    : BackToTopBtn.classList.remove("show");
 
   //   active the link according to the current section
 
@@ -74,7 +86,6 @@ navLinks.forEach((link) => {
   });
 });
 
-const skillsCard = document.querySelectorAll(".skill-card");
 const skillObserver = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
@@ -88,7 +99,7 @@ const skillObserver = new IntersectionObserver(
     });
   },
   {
-    threshold: .5,
+    threshold: 0.5,
   },
 );
 
@@ -96,7 +107,67 @@ skillsCard.forEach((skill) => {
   skillObserver.observe(skill);
 });
 
-
-
 // ---  Form valid code
 
+form.addEventListener("submit", (val) => {
+  val.preventDefault();
+  let isNameCorrect = false;
+  let isMailCorrect = false;
+  let isMessageCorrect = false;
+  const nameRegex = /^(?=.{3,50}$)[A-Za-z]+(?: [A-Za-z]+)*$/;
+  const mailRegex = /^[^\s@]{2,}@[^\s@]{2,}\.[a-zA-Z]{2,}$/;
+  const messageRegex = /^.{10,600}$/s;
+
+  // functions for validation
+  function trueValidation(input, message) {
+    input.classList.remove("error");
+    message.textContent = "";
+  }
+  function falseValidation(input, message, messageText) {
+    input.classList.add("error");
+    input.focus();
+    message.textContent = messageText;
+  }
+
+  // check the full name validation
+  if (nameRegex.test(fullNameInput.value.trim())) {
+    isNameCorrect = true;
+    trueValidation(fullNameInput, nameError);
+  } else {
+    isMailCorrect = false;
+    falseValidation(fullNameInput, nameError, "Please Enter your valid name");
+  }
+
+  // check the mail validation
+  if (mailRegex.test(mailInput.value.trim())) {
+    isMailCorrect = true;
+    trueValidation(mailInput, mailError);
+  } else {
+    isMailCorrect = false;
+    falseValidation(mailInput, mailError, "Please Enter valid Mail");
+  }
+
+  // check the message validation
+  if (messageRegex.test(messageTextarea.value.trim())) {
+    isMessageCorrect = true;
+    trueValidation(messageTextarea, messageError);
+  } else {
+    isMessageCorrect = false;
+    falseValidation(
+      messageTextarea,
+      messageError,
+      "Message must be valid and at least 10 characters",
+    );
+  }
+
+  // if all true then submit the form
+  if (isNameCorrect && isMailCorrect && isMessageCorrect) {
+    submittedMessage.style.display = "block";
+    fullNameInput.value = "";
+    mailInput.value = "";
+    messageTextarea.value = "";
+    setInterval(() => {
+      submittedMessage.style.display = "none";
+    }, 5000);
+  }
+});
